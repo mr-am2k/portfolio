432a08af69e97d9c13435100135f45ecdfefb088
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-06 18:12:31 +0200

    Created starting point for new homepage design

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,6 +2,14 @@ import classes from './HomePage.module.css';
 const HomePage = () => {
     return (
         <div className={classes.homePage}>
+            <h4>Let's work together</h4>
+            <hr />
+            <h1>Hello there,👋 I'm <span>Muamer</span> </h1>
+            <h1>Software engineering student and </h1>
+            <h1>Web developer</h1>
+            <p>I'm software engineering student passionate about web development.
+                Currently I'm working on some personal projects to improve my knowledge about both, front-end and back-end.
+            </p>
         </div>
     )
 }
