0dbba586e40fe64c210df936c012a7ff1ae88740
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-06 21:31:32 +0200

    Completed home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Fragment } from 'react'
 import { Navbar, HomePage } from './components/index'
+import classes from './App.module.css'
 const App = () => {
   return (
     <Fragment>
+      <div className={classes.navbar}>
+
       <Navbar />
-      <HomePage />
+      </div>
+      <div className={classes.container}>
+        <HomePage />
+        <p>HE</p>
+      </div>
     </Fragment>
   )
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,8 +4,7 @@ const HomePage = () => {
         <div className={classes.homePage}>
             <h4>Let's work together</h4>
             <h1>Hello there,👋 I'm <span>Muamer</span> </h1>
-            <h1>Software engineering student and </h1>
-            <h1>Web developer</h1>
+            <h1>Software engineering student and <br/> Web developer </h1>
             <p>I'm software engineering student passionate about web development.
                 Currently I'm working on some personal projects to improve my knowledge about both, front-end and back-end.
             </p>
