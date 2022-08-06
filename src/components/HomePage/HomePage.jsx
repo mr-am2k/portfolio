c59f285d564155109b54fc2c0bed54600ed5ff77
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-06 18:28:51 +0200

    Created home page component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,13 +3,15 @@ const HomePage = () => {
     return (
         <div className={classes.homePage}>
             <h4>Let's work together</h4>
-            <hr />
             <h1>Hello there,👋 I'm <span>Muamer</span> </h1>
             <h1>Software engineering student and </h1>
             <h1>Web developer</h1>
             <p>I'm software engineering student passionate about web development.
                 Currently I'm working on some personal projects to improve my knowledge about both, front-end and back-end.
             </p>
+            <a href='../../assets/muamercv.pdf' download="Muamer's resume">
+                <button type='button'>Download Resume</button>
+            </a>
         </div>
     )
 }
